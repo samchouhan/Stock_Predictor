b606b401e497015234048716a3f2bc5d4da5f498
SAKSHAM_CHOUHAN <131700345+samchouhan@users.noreply.github.com>
2026-04-19 11:03:03 +0530

    Change stock data source from AAPL to ADANIPOWER.NS

--- Stock_Predictor_1.py
+++ Stock_Predictor_1.py
@@ -6,7 +6,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 
 # Step 1: Get stock data
-stock = yf.download("AAPL", start="2020-01-01", end="2024-01-01")
+stock = yf.download("ADANIPOWER.NS", start="2020-01-01", end="2024-01-01")
 
 # Step 2: Keep only closing price
 data = stock[['Close']]
