4130091670878f4f11949bbd0bdf739fb2bde5a9
samchouhan <sakshamchouhan8@gmail.com>
2026-09-12 23:35:52 +0530

    Refactor code for consistency in bracket usage and formatting

--- Stock_Predictor_1.py
+++ Stock_Predictor_1.py
@@ -20,15 +20,15 @@ else:
     stock = yf.download("ADANIPOWER.NS", start="2025-01-01", end="2026-04-01")
 
 # Step 2: Keep only closing price
-data = stock[['Close']]
+data = stock[["Close"]]
 
 # Step 3: Create future prediction column
 future_days = 30
-data['Prediction'] = data[['Close']].shift(-future_days)
+data["Prediction"] = data[["Close"]].shift(-future_days)
 
 # Step 4: Prepare data
-X = np.array(data.drop(['Prediction'], axis=1))[:-future_days]
-y = np.array(data['Prediction'])[:-future_days]
+X = np.array(data.drop(["Prediction"], axis=1))[:-future_days]
+y = np.array(data["Prediction"])[:-future_days]
 
 # Step 5: Train-test split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
@@ -38,15 +38,17 @@ model = LinearRegression()
 model.fit(X_train, y_train)
 
 # Step 7: Predict future prices
-x_future = data.drop(['Prediction'], axis=1).tail(future_days)
+x_future = data.drop(["Prediction"], axis=1).tail(future_days)
 x_future = np.array(x_future)
 
 predictions = model.predict(x_future)
 
 # Step 8: Plot
-plt.figure(figsize=(10,5))
-plt.plot(data['Close'], label="Actual Price")
-plt.plot(range(len(data), len(data)+future_days), predictions, label="Predicted Price")
+plt.figure(figsize=(10, 5))
+plt.plot(data["Close"], label="Actual Price")
+plt.plot(
+    range(len(data), len(data) + future_days), predictions, label="Predicted Price"
+)
 plt.legend()
 plt.show()
 
