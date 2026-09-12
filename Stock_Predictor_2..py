1b62b2ab4b914a3fed4377245afe13160e2d4777
samchouhan <sakshamchouhan8@gmail.com>
2026-09-12 23:56:54 +0530

    Add stock data download and user input for time period in Stock_Predictor_2.py

--- Stock_Predictor_2..py
+++ Stock_Predictor_2..py
@@ -6,3 +6,9 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 import yfinance as yf
 
+#Download stock data
+ticker = "JISLJALEQS.NS"
+TIME = input("Enter time period (5y / 1y / 1m): ").strip().lower()
+
+period_map = {
+    "5y": ("2021-01-01", "2026-04-18"),
