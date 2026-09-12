cc3fb66663acd0052d4ccfa2d1b30cf89cb0afac
samchouhan <sakshamchouhan8@gmail.com>
2026-09-12 23:55:32 +0530

    Fix formatting by adding a newline at the end of Stock_Predictor_2.py

--- Stock_Predictor_2..py
+++ Stock_Predictor_2..py
@@ -5,3 +5,4 @@ from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 import yfinance as yf
+
