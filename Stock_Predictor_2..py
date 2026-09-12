4183ecaec5e7bba9e1719c470e4bda5603d12717
samchouhan <sakshamchouhan8@gmail.com>
2026-09-12 23:54:44 +0530

    Add initial project structure with Python version and main script

---
+++ Stock_Predictor_2..py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+import yfinance as yf
